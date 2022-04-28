431ae60dda2617e0c59d1f57620dcc5584bc0de3
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-28 11:52:16 +0200

    added bootstrap and animate

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+import "bootstrap/dist/css/bootstrap.css"
+
+import "bootstrap/dist/js/bootstrap.js"
+import 'animate.css';
