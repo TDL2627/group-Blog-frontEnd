e005f5c2e0b224fd56c06a8b4e61fc1b436582cb
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-28 14:29:29 +0200

    added add blog

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -25,7 +25,12 @@
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-      
+    
+<li class="nav-item">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+ CREATE BLOG
+</button>
+        </li>
          <li class="nav-item">
             <router-link class="nav-link" to="/">LOGOUT</router-link>
         </li>
@@ -35,6 +40,40 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+
+      <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">CREATE BLOG</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+  
+  <div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">Title</label>
+  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput2" class="form-label">Body</label>
+  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput3" class="form-label">Image URL</label>
+  <input type="text" class="form-control" id="formGroupExampleInput3" placeholder="Another input placeholder">
+</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   </div>
 </nav>
