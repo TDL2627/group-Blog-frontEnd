bae079a6f71b25279200810f16108025f31e3767
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-28 13:53:58 +0200

    added sign up form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="login">
 
+      <h1>LOGIN</h1>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   </div>
 </template>
 
@@ -11,5 +25,9 @@ export default {
 </script>
 
 <style scoped>
-
+form{
+    border: 5px solid black;
+    margin: 5%;
+    padding: 5%;
+}
 </style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="signup">
+      
+      <h1>SIGN UP</h1>
+<form>
+     <div class="mb-3">
+    <label for="name" class="form-label">Full Name</label>
+    <input type="text" class="form-control" id="name">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+form{
+    border: 5px solid black;
+    margin: 5%;
+    padding: 5%;
+}
+</style>
