ecbdc985da5200cf1d0fc1837eebbfaeaea82242
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-28 12:46:31 +0200

    basic landing

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="landing">
 
-    <h1>SUPER BLOG</h1>
+    <h1 class="animate__animated animate__bounce">SUPER BLOG</h1>
+
+    <div class="container">
+      <div class="row">
+    <div class="col-lg-6">
+        <router-link to="/login">LOG IN</router-link>
+      </div>
+           <div class="col-lg-6">
+        <router-link to="/signup">SIGN UP</router-link>
+      </div>
+      </div>
+  
+    </div>
 
   </div>
 </template>
@@ -13,5 +25,15 @@ export default {
 </script>
 
 <style scoped>
-
+#landing{
+  padding-top:10% ;
+}
+.container{
+  padding-top: 20%;
+}
+a{
+  text-decoration: none;
+  border: 2px solid black;
+  padding: 20px;
+}
 </style>
